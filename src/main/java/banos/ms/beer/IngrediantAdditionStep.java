e721c72f5af21837f44a218ff082c40ae29adb13
Banoshe <msbanos@yahoo.com>
2018-10-01 15:03:33 -0600

    enum for when an ingrediant is used

---
+++ src/main/java/banos/ms/beer/IngrediantAdditionStep.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/01/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The step in which an ingrediant is used.
+ */
+public enum IngrediantAdditionStep { MASH, BOIL, VORLAUF }
