c96e36d1c7803e113c011a8df56846784b8ed220
Banoshe <msbanos@yahoo.com>
2018-10-14 16:55:26 -0600

    flaw level enum

---
+++ src/main/java/banos/ms/beer/FlawLevel.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/14/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The beer flaw level.
+ */
+public enum FlawLevel { LOW, MEDIUM, HIGH }
