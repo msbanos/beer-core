40476ac6178388ddad3fdc85c754589d97eb2aa5
Banoshe <msbanos@yahoo.com>
2018-10-14 17:01:33 -0600

    flaw enum

---
+++ src/main/java/banos/ms/beer/Flaw.java
@@ -0,0 +1,31 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/14/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		A beer flaw.
+ */
+public enum Flaw {
+	ACETALDEHYDE,
+	ALCOHOLIC_HOT,
+	ASTRINGENT,
+	BRETTANOMYCES,
+	DIACETYL,
+	DMS,
+	ESTERY,
+	GRASSY,
+	LIGHT_STRUCK,
+	MEDICINAL,
+	METALLIC,
+	MUSTY,
+	OXIDIZED,
+	PLASTIC,
+	SOLVENT_FUSEL,
+	SOUR_ACIDIC,
+	SMOKEY,
+	SPICEY,
+	SULFUR,
+	VEGETAL
+}
