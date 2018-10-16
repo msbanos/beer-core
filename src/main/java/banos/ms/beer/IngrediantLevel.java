f40c4b3c131fc2e81142f9249e1851a31adfd3eb
Banoshe <msbanos@yahoo.com>
2018-10-15 21:04:13 -0600

    percieved ingrediant level

---
+++ src/main/java/banos/ms/beer/IngrediantLevel.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/15/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The perceived ingrediant level.
+ */
+public enum IngrediantLevel { NONE, LOW, MEDIUM, HIGH }
