69a55a249a49b82cdb3ea0562392a82f88b686a9
Banoshe <msbanos@yahoo.com>
2018-10-16 08:12:31 -0600

    beer color enujm

---
+++ src/main/java/banos/ms/beer/BeerColor.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/16/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The possible beer colors.
+ */
+public enum BeerColor { YELLOW, GOLD, AMBER, COPPER, BROWN, BLACK }
