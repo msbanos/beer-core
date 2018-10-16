fc1a0b9b2aefd5e9804f2e3a1ef046620cc56006
Banoshe <msbanos@yahoo.com>
2018-10-16 09:03:18 -0600

    clarity enum

---
+++ src/main/java/banos/ms/beer/Clarity.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/16/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The possible beer clarity descriptions.
+ */
+public enum Clarity { BRILLIANT, HAZY, OPAQUE }
