6e4a8d1723eea682f0a0cec4f44bb291fa300882
Banoshe <msbanos@yahoo.com>
2018-10-17 09:01:24 -0600

    beer body

---
+++ src/main/java/banos/ms/beer/BeerBody.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/17/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The possible beer bodies.
+ */
+public enum BeerBody { THIN, MEDIUM, FULL }
