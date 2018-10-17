b8afd701690fb3111403d71471bea905b107f078
Banoshe <msbanos@yahoo.com>
2018-10-17 08:13:15 -0600

    flavor balance enum

---
+++ src/main/java/banos/ms/beer/FlavorBalance.java
@@ -0,0 +1,10 @@
+package banos.ms.beer;
+
+/**
+ * [Author]			Mike Banos
+ * [Date]			10/17/2018
+ * [Licensing ]		All works licensed by the Lesser GPL
+ * 		@link http://www.gnu.org/licenses/lgpl.html
+ * [Summary]		The possible flavor balances.
+ */
+public enum FlavorBalance { HOPPY, I, MALTY }
